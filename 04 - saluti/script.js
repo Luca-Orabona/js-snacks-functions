fbc21c780031779a44c4d814e0e1fa8111348ccd
Luca-Orabona <lucaorabona94@gmail.com>
2025-04-23 20:26:00 +0200

    feat: Saluti completed

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,16 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluto(string) {
 
+    let salutoString = `ciao ${string}`;
+
+    return salutoString
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = saluto(name);
+console.log("Saluto:", "''''", result, "''''");
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
