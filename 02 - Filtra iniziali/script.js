26aabb1d2f683222809298afd0c4ecd5e829e84d
Luca-Orabona <lucaorabona94@gmail.com>
2025-04-23 19:56:58 +0200

    feat: Filtra iniziali completed

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,25 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+const letteraIniziale = "a";
 
 // Dichiara la funzione qui.
+function nomiLetteraIniziale(array, letteraIniziale) {
+
+    const namesLetteraIniziale = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0].toUpperCase() === letteraIniziale.toUpperCase()) {
+            namesLetteraIniziale.push(array[i]);
+        }
+    }
+    // console.log(namesLetteraIniziale);
+    return namesLetteraIniziale;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = nomiLetteraIniziale(names, letteraIniziale);
+console.log(result);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
