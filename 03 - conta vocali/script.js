49ccd74c96aa2d4babe8564972b0f217aa86d73a
Luca-Orabona <lucaorabona94@gmail.com>
2025-04-23 20:12:12 +0200

    feat: Conta vocali completed

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,9 +4,30 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function numVocali(string) {
+    const vocali = "aeiouAEIOU";
+    conteggioVocali = 0;
 
+    for (let i = 0; i < string.length; i++) {
+        // for (let j = 0; j < vocali.length; j++){
+        //     if (string[i] === vocali[j]) {
+        //         conteggioVocali++
+
+        //     }
+
+        // }
+
+        if (vocali.includes(string[i])) {
+            conteggioVocali++
+        }
+    }
+
+    return conteggioVocali
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = numVocali(word);
+console.log("Il numero di vocali è:", result);
 
 
 
